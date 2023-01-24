ff1040ca564e24284d91e28f6772a38a46440261
suraj kumar <surajkumarnishad1998@gmail.com>
2023-01-24 16:25:28 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import '../css/global.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import {Link} from 'react-router-dom'
+
+const MyHome=()=>{
+
+	return(
+		<>
+			<nav>
+				<ul>
+					<li><Link to="/home">Home</Link></li>
+					<li><Link to="/About">About</Link></li>
+					<li><Link to="/Contact">Contact</Link></li>
+					<li><Link to="/Courses">Courses</Link></li>
+					<li><Link to="/Services">Services</Link></li>
+				</ul>
+			</nav>
+			<div id="carouselExampleCaptions" className="carousel slide">
+  <div className="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="images/slider2.jpg" className="d-block w-100" alt="..."/>
+      <div className="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div className="carousel-item">
+      <img src="images/slider4.jpg" className="d-block w-100" alt="..."/>
+      <div className="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div className="carousel-item">
+      <img src="images/slider5.jpg" className="d-block w-100" alt="..."/>
+      <div className="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+		</>
+		)
+
+}
+export default MyHome
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import MyHome from './components/Home'
+import {About} from './components/About'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  
-    
+  <BrowserRouter>
+    <Routes>
+    <Route path='' element={<MyHome/>}/>
+
+    <Route path='Home' element={<MyHome/>}>
+
+    <Route path='About' element={<About/>}/>
+
+    </Route>
+
+
+    </Routes>
+  </BrowserRouter>
   </React.StrictMode>
 );
